b4909a9b4a637a761d31592ff5d1fe850640da15
2oofine <it.sherwinromero@gmail.com>
2025-06-07 19:46:23 +0800

    allow xrobot header index

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Other config options here if you have any
+
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow", // ✅ allow indexing
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
