b45a9c74ccd379d5af002c56acba1d64cb680baa
2oofine <it.sherwinromero@gmail.com>
2025-06-09 03:00:58 +0800

    change animation

--- src/pages/home/sections/projects/index.tsx
+++ src/pages/home/sections/projects/index.tsx
@@ -18,7 +18,7 @@ const ProjectsSection = () => {
 
   return (
     <Section className="py-10">
-      <motion.div variants={fadeInUp} initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.2 }}>
+      <motion.div variants={fadeInUp} initial="hidden" animate="visible">
         <div className="">
           <h2 className="text-3xl font-bold">A Peek Into My Work</h2>
           <h5 className="mb-6">Some projects I&apos;ve been working on</h5>
